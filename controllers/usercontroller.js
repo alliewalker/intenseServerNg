90839b913d6e01170f0e01c17b6cefd57500c4f9
jess-YD <jess.diego26@gmail.com>
2019-02-22 15:22:15 -0500

    trip

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -12,10 +12,14 @@ let jwt = require("jsonwebtoken");
 router.post("/create", function (req, res) { //ok to receive a post request
     let email = req.body.user.email;
     let password = req.body.user.password;
+    let adminStatus = req.body.user.adminStatus;
+    let userRole = req.body.user.userRole;
     //console.log(req.body)
     User.create({
         email: email,
         passwordhash: bcrypt.hashSync(password, 10),  //this means you will do 10 rounds of bycrpt.
+        adminStatus: adminStatus,
+        userRole: userRole
     }).then(
         function createSuccess(user) {
             let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
--- models/users.js
+++ models/users.js
@@ -15,7 +15,9 @@ module.exports = function( sequelize, DataTypes) {
         userRole: {               //IN MY CONTROLLER ASSIGN VALUE TO THIS
             type: DataTypes.STRING,
         },
-        adminStatus: DataTypes.BOOLEAN
+        adminStatus: {
+           type: DataTypes.BOOLEAN
+        }
     })
 }
 
