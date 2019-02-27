70b1bfc9f673b44ba2e3d9f50d35bc701f21041a
Justis Gipson <justis710@gmail.com>
2019-02-26 19:23:08 -0500

    trip.js fix

--- models/trip.js
+++ models/trip.js
@@ -1,11 +1,7 @@
 module.exports = function( sequelize, DataTypes) {
     const Trip = sequelize.define("trips", { //what I put is the name of my table in postgres
         date: {
-<<<<<<< HEAD
             type: DataTypes.RANGE(DataTypes.DATEONLY), //YEAR - MONTH - DAY!
-=======
-            type: DataTypes.RANGE(DataTypes.DATEONLY),
->>>>>>> aa16e5cb45dd945f42497cf8276d8e4043a75f86
             notEmpty: true
         },
         location: {
